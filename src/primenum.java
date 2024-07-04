38f3ac7f2e9c7b69eeab098b3a085cfedbd88a35
Vishaln_2004 <vishalitisthatis2004@gmail.com>
2024-07-05 00:24:24 +0530

    commit

--- src/primenum.java
+++ src/primenum.java
@@ -1,2 +1,24 @@
+import java.util.*;
+
 public class primenum {
-}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Boolean ans = isprime(n);
+        System.out.print(ans);
+    }
+        static boolean isprime(int n){
+
+
+        int c=2;
+        while(c*c<=n){
+            if(n%c==0){
+                return false;
+            }
+            c++;
+        }
+        return true;
+    }
+    }
+
+
