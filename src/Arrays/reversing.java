6e065355d5b7ee5abd027167351ed4e2c9f4fcd4
Vishaln_2004 <vishalitisthatis2004@gmail.com>
2024-07-06 17:11:51 +0530

    commit

---
+++ src/Arrays/reversing.java
@@ -0,0 +1,20 @@
+package Arrays;
+
+public class reversing {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new arr[n];
+
+        for(int i=0; i<n-1; i++) {
+            arr[i] = sc.nextInt();
+        }
+    }
+    static void reverse(int[] arr, int start, int end){
+        for(int i=0; i<n-1; i++){
+            if(start < end){
+
+            }
+        }
+    }
+}
